83020e722aeb5449d99aa64b904f720547bca317
GI_Jack <none>
2017-12-26 00:11:17 -0500

    updated now srm command is a variable, and now a exit_with_error_soft() that does not have a dialog window.

--- srm_guified.sh
+++ srm_guified.sh
@@ -5,7 +5,12 @@
 #
 #  licensed under the GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 
-# see man srm. default is two pass 0xFF then random
+# Exit codes 0-success 2-script_failure 4-user_failure Anything else:
+# srm failure
+
+# the default is to use srm from THC's secure-delete, using two passes random,
+# with sync and /dev/urandom.
+SRM_PROG="srm"
 SRM_OPTS="lr"
 
 DEP_LIST="srm Xdialog notify-send"
@@ -20,6 +25,12 @@ exit_with_error(){
   exit ${1}
 }
 
+exit_with_error_soft(){
+  echo 1>&2 "srm_guified.sh: ERROR: ${2}"
+  notify-send --icon shred "Secure Delete" "${2} (${1})"
+  exit ${1}
+}
+
 message(){
   echo "srm_guified.sh: ${@}"
 }
@@ -29,7 +40,7 @@ check_deps(){
   for dep in ${DEP_LIST};do
     which ${dep} &> /dev/null
     if [ $? -ne 0 ];then
-      exit_with_error 4 "$dep is not in \$PATH! This is needed to run. Quitting"
+      exit_with_error 2 "$dep is not in \$PATH! This is needed to run. Quitting"
     fi
   done
 }
@@ -94,7 +105,7 @@ run_delete() {
       # line at the end, incrementing the counter.
       for file in ${ALL_FILES};do
         echo $(( ${counter} / 1000 )) # convert back to percent
-        srm -${SRM_OPTS} "${file}"
+        ${SRM_PROG} -${SRM_OPTS} "${file}"
         exit_code+=${?}
         [ $counter -ge 100000 ] && continue # percent stops at 100, silly
         counter+=${step}
@@ -102,7 +113,7 @@ run_delete() {
       sleep ${fin_wait}
     ) |
     Xdialog --icon shred --title "Secure Delete" --gauge "Wiping ${NUM_FILES} file(s)" ${win_height} ${win_length}
-    [ $? -eq 255 ] && exit_with_error 4 "Secure Wipe Canceled"
+    [ $? -eq 255 ] && exit_with_error 4 "Secure Wipe Aborted"
     ;;
   esac
   return ${exit_code}
@@ -118,8 +129,7 @@ main() {
     run_delete
     exit_code=${?}
    else
-    echo "user canceled"
-    exit 2
+    exit_with_error_soft 4 "User Canceled"
   fi
   # Then notify the user the result
   notify_complete ${exit_code}
