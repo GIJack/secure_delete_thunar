67d1cd34e02e48bdbac33c62a42cb10d70208eb8
GI_Jack <none>
2019-05-30 20:51:51 -0400

    added better exit/error handling

--- srm_guified.sh
+++ srm_guified.sh
@@ -5,8 +5,7 @@
 #
 #  licensed under the GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 
-# Exit codes 0-success 2-script_failure 4-user_failure Anything else:
-# srm failure
+# Exit codes 0-success 1-srm_failure 2-script_failure 4-user_failure
 
 # the default is to use srm from THC's secure-delete, using two passes random,
 # with sync and /dev/urandom.
@@ -23,24 +22,26 @@ DEP_LIST="srm Xdialog notify-send"
 CONFIRM="N"
 ICON="shred"
 
+declare -i FILE_FAILS=0
+
 exit_with_error(){
   local -i win_length=45
   local -i win_height=8
   echo 1>&2 "srm_guified.sh: ERROR: ${2}"
-  notify-send --icon ${ICON} "Secure Delete" "${2} (${1})"
+  notify-send --icon ${ICON} "Secure Delete: ERROR!" "${2} (${1})"
   Xdialog --icon ${ICON} --title "Secure Delete" --msgbox "${2} (${1})" ${win_height} ${win_length}
   exit ${1}
 }
 
 exit_with_error_soft(){
   echo 1>&2 "srm_guified.sh: ERROR: ${2}"
-  notify-send --icon ${ICON} "Secure Delete" "${2} (${1})"
+  notify-send --icon ${ICON} "Secure Delete: ERROR!" "${2} (${1})"
   exit ${1}
 }
 
 warn_notify(){
   echo 1>&2 "srm_guified.sh: Warn: ${@}"
-  notify-send --icon ${ICON} "Secure Delete" "${@}"
+  notify-send --icon ${ICON} "Secure Delete: Warning!" "${@}"
 }
 
 message(){
@@ -86,15 +87,15 @@ notify_complete() {
   # libnotify end results
   local -i win_length=45
   local -i win_height=8
-  local -i exit_code=${1}
-  case ${exit_code} in
+  local -i complete_files=$((${NUM_FILES} - ${FILE_FAILS}))
+  case ${FILE_FAILS} in
    0)
     notify-send --icon ${ICON} "Secure Delete" "Finished Securely Deleting ${NUM_FILES} File(s)"
     exit 0
     ;;
    *)
-    exit_with_error ${exit_code} "Secure Wipe Failed!"
-    exit ${exit_code}
+    notify-send --icon ${ICON} "Secure Delete" "Finished Securely Deleted ${complete_files} File(s). Unable to delete ${FILE_FAILS} file(s)"
+    exit 1
     ;;
   esac
 }
@@ -103,7 +104,6 @@ run_delete() {
   local -i win_length=45
   local -i win_height=8
   local -i exit_code=0
-  local -i i_exit=0
   local -i step=$(( 100000 / ${NUM_FILES} )) # Per 100,000. Convert later
   local -i counter=0
   local fin_wait=0.5 #time in seconds to wait after finnishing
@@ -120,9 +120,10 @@ run_delete() {
       for file in "${ALL_FILES[@]}";do
         echo $(( ${counter} / 1000 )) # convert back to percent
         ${SRM_PROG} ${SRM_OPTS} "${file}"
-        i_exit=${?}
-        [ ${i_exit} -ne 0 ] && warn_notify "Could not delete ${file}!"
-        exit_code+=${i_exit}
+        if [ ${?} -ne 0 ];then
+          warn_notify "Could Not Delete ${file}!"
+          FILE_FAILS+=1  
+        fi
         [ $counter -ge 100000 ] && continue # percent stops at 100, silly
         counter+=${step}
       done
@@ -136,18 +137,16 @@ run_delete() {
 }
 
 main() {
-  local -i exit_code=0
   declare -i NUM_FILES=$#
   declare ALL_FILES=("${@}")
   confirm_delete
   # do the wipe
   if [ ${CONFIRM} == "Y" ];then
     run_delete
-    exit_code=${?}
    else
     exit_with_error_soft 4 "User Canceled"
   fi
   # Then notify the user the result
-  notify_complete ${exit_code}
+  notify_complete
 }
 main "${@}"
